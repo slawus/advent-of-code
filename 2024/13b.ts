03846ca48ca4cf85c60a311d2ec1f88c69132ed2
Slawek Pruchnik <slawek@streamsage.io>
2024-12-13 12:14:36 +0100

    2024/13

---
+++ 2024/13b.ts
@@ -0,0 +1,47 @@
+function processInput(input: string) {
+  const taskStr = input.split("\n\n")
+
+  return taskStr.map(str => {
+    const [, ax, ay] = str.match(/Button A: X\+(\d+), Y\+(\d+)/)
+    const [, bx, by] = str.match(/Button B: X\+(\d+), Y\+(\d+)/);
+    const [, X, Y] = str.match(/Prize: X=(\d+), Y=(\d+)/);
+
+    return {
+      ax: Number(ax),
+      bx: Number(bx),
+      ay: Number(ay),
+      by: Number(by),
+      X: Number("10000000000000") + Number(X),
+      Y: Number("10000000000000") + Number(Y),
+    }
+  })
+
+}
+
+function main(text: string) {
+
+  const tasks = processInput(text);
+
+  const moves = tasks.map(({ X, Y, bx, by, ay, ax }) => {
+    const a = (X*by - Y * bx) / (ax*by - ay * bx);
+    const b = (Y - ay* a) / by;
+
+
+    if(!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+      // not valid
+      return null;
+    }
+
+    return [a, b];
+  })
+
+  const sum = moves.filter(m => m !== null).reduce((acc, [a, b]) => acc + BigInt(a) * BigInt(3) + BigInt(b), BigInt(0));
+  return {
+    sum
+  }
+}
+
+import * as fs from "node:fs";
+
+const input = fs.readFileSync("./inputs/13.txt", "utf8");
+console.log(main(input));
